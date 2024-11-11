01d88afdcc91b4a373442369e223ab480864bc87
Debajit Ghosh <dg@dejabotz.com>
2024-11-10 19:46:06 -0800

    formatting fixes

--- static/js/ref_display.js
+++ static/js/ref_display.js
@@ -10,29 +10,31 @@ var handleMatchLoad = function(data) {
 
 // Handles a websocket message to update the match time countdown.
 var handleMatchTime = function(data) {
-    translateMatchTime(data, function(matchState, matchStateText, countdownSec) {
-      $("#matchState").text(matchStateText);
-      $("#matchTime").text(countdownSec);
+    translateMatchTime(data, function (matchState, matchStateText, countdownSec) {
+        $("#matchState").text(matchStateText);
+        $("#matchTime").text(countdownSec);
     });
-  };
+};
 
 // Handles a websocket message to update the match score.
 var handleRealtimeScore = function(data) {
-  if (alliance === "red") {
-    $("#score").text(data.Red.ScoreSummary.Score);
-  } else if (alliance === "blue") {
-    $("#matchScore").text(data.Blue.ScoreSummary.Score);
-  }
+    // update the overall score
+    if (alliance === "red") {
+        $("#score").text(data.Red.ScoreSummary.Score);
+    } else if (alliance === "blue") {
+        $("#matchScore").text(data.Blue.ScoreSummary.Score);
+    }
+
+    // 
 };
 
-$(function() {  
+$(function() {
     alliance = window.location.href.split("/").slice(-1)[0];
     // Set up the websocket back to the server.
     websocket = new CheesyWebsocket("/displays/ref/" + alliance + "/websocket", {
-      matchLoad: function(event) { handleMatchLoad(event.data); },
-      matchTiming: function(event) { handleMatchTiming(event.data); },
-      matchTime: function(event) { handleMatchTime(event.data); },
-      realtimeScore: function(event) { handleRealtimeScore(event.data); },
+        matchLoad: function (event) { handleMatchLoad(event.data); },
+        matchTiming: function (event) { handleMatchTiming(event.data); },
+        matchTime: function (event) { handleMatchTime(event.data); },
+        realtimeScore: function (event) { handleRealtimeScore(event.data); },
     });
-  });
-  
+});
