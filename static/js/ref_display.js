a4a63712cf559d935788c958f3879c046381a049
Debajit Ghosh <dg@dejabotz.com>
2024-11-10 20:04:23 -0800

    finish initial plumbing for updating score elements from websocket messages

--- static/js/ref_display.js
+++ static/js/ref_display.js
@@ -18,14 +18,38 @@ var handleMatchTime = function(data) {
 
 // Handles a websocket message to update the match score.
 var handleRealtimeScore = function(data) {
+
+    var score;
+
     // update the overall score
     if (alliance === "red") {
         $("#score").text(data.Red.ScoreSummary.Score);
+        score = data.Red.Score;
     } else if (alliance === "blue") {
         $("#matchScore").text(data.Blue.ScoreSummary.Score);
+        score = data.Blue.Score;
     }
 
-    // 
+    // update each of the individal scoring elements
+    // taxi
+    $("#taxi1").text(score.Taxi[0]);
+    $("#taxi2").text(score.Taxi[1]);
+
+    // shelf
+    $("#auton_bottom").text(score.Shelf.AutonBottomShelfCubes);
+    $("#auton_top").text(score.Shelf.AutonTopShelfCubes);
+    $("#teleop_bottom").text(score.Shelf.TeleopBottomShelfCubes);
+    $("#teleop_top").text(score.Shelf.TeleopTopShelfCubes);
+
+    // golden_cube
+    $("#golden_cube").text(score.GoldenCube);
+
+    // hamper
+    $("#hamper").text(score.Hamper);
+
+    // park
+    $("#park1").text(score.Park[0]);
+    $("#park2").text(score.Park[1]);
 };
 
 $(function() {
